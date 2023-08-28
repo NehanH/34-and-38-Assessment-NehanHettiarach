d6a1aa2536867a5388597e53bffb8e2584322028
NehanH <69770862+NehanH@users.noreply.github.com>
2023-08-28 03:47:06 +0000

    V16: Navbar implemented, Now working on responsiveness

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ $(document).ready(function() {
 window.onload = function() {
 document.querySelector('.welcome').classList.add('welcome-animation');
 };
+
+// NAVBAR JS CREATED BY Håvard Brynjulfsen
+// NAVBAR EDITED BY NEHAN HETTIARACHCHI TERM 3 2023
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "https://unpkg.com/akar-icons-fonts/src/css/akar-icons.css";
+head.appendChild(link);
