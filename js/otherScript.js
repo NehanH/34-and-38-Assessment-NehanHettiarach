90b4aeb0a5601978e03740e00f2d4f14920babeb
NehanH <69770862+NehanH@users.noreply.github.com>
2023-10-25 02:47:32 +0000

    V18: Final Version

--- js/otherScript.js
+++ js/otherScript.js
@@ -4,3 +4,33 @@ $(document).ready(function() {
     $('.container').toggleClass('menu-open');
   })
 })
+
+// NAVBAR JS CREATED BY Håvard Brynjulfsen
+// NAVBAR EDITED BY NEHAN HETTIARACHCHI TERM 3 2023
+var head = document.getElementsByTagName("head")[0];
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "https://unpkg.com/akar-icons-fonts/src/css/akar-icons.css";
+head.appendChild(link);
+
+$(document).ready(function() {
+  if (sessionStorage.getItem('visited')){
+    $('#welcome').removeClass('welcome-animation');
+    $('.title, .subtitle, .grid-item, .toggle, .navbar-container').addClass('immediate-visibility');
+  } else {
+    $('#welcome').addClass('welcome-animation');
+  }
+
+  $(".ai-home").parent().click(function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('visited', true);
+    window.location.href= "../index.html"
+  });
+
+    $(".nav-home").parent().click(function(e) {
+    e.preventDefault();
+    sessionStorage.setItem('visited', true);
+    window.location.href= "../index.html"
+  });
+});
